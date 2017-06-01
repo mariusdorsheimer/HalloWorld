5aaebb2d05f15c5ad8269553cdd17d3dd8a912a3
Sid <m.dorsheimer@one-dot.de>
2017-06-01 11:13:54 +0200

    update

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -10,6 +10,6 @@ class ContentController extends Controller {
 
     public function sayHello(Twig $twig):string {
 
-        return $twig->render('HelloWorld::content.helloWorld');
+        return $twig->render('HalloWorld::content.helloWorld');
     }
 }
