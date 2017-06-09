8701695dcb75f18e9e5ed9d3f06bbeb2e1f5fb1a
sid <m.dorsheimer@one-dot.de>
2017-06-09 11:31:01 +0200

    update

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -2,13 +2,20 @@
 
 namespace HelloWorld\Controllers;
 
+use Plenty\Plugin\ConfigRepository;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Log\Loggable;
+
 
 
 class ContentController extends Controller {
 
-    public function sayHello(Twig $twig) {
+    use Loggable;
+
+    public function sayHello(Twig $twig, ConfigRepository $repository) {
+
+        $this->getLogger('test')->error('test', $repository->get('HelloWorld.testKey'));
 
         return $twig->render('HelloWorld::content.helloWorld');
     }
